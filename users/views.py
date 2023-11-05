83192b2d1a3a3e979a00f4c71d7ea77c59a4582d
Евгений <xarvat@list.ru>
2023-11-05 13:24:06 +0300

    new project new app habits create users

---
+++ users/views.py
@@ -0,0 +1,42 @@
+from rest_framework import generics
+
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    """ Создание нового пользователя """
+
+    serializer_class = UserSerializer
+
+
+class UserListAPIView(generics.ListAPIView):
+    """ Список пользователей """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    """ Информация о пользователе """
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    """ Обновление информации о пользователе """
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    """ Удаление пользователя """
+
+    queryset = User.objects.all()
+
+
+from django.shortcuts import render
+
+# Create your views here.
