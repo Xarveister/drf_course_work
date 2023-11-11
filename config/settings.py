d19e4f79e0bf1b4eb86e3c194801892eee6dec2f
Евгений <xarvat@list.ru>
2023-11-11 17:05:06 +0300

    add tests

--- config/settings.py
+++ config/settings.py
@@ -36,10 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_celery_beat',
+
     'rest_framework',
     'rest_framework_simplejwt',
-
+    'django_celery_beat',
     'corsheaders',
     'drf_yasg',
     'habits',
@@ -162,8 +162,8 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ALLOW_ALL_ORIGINS = False
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://localhost:127.0.0.1:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:127.0.0.1:6379'
 CELERY_TIMEZONE = "Europe/Moscow"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
